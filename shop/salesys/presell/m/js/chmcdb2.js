b24071c179dbedc575350ff1fa9ff9cb13b5af38
homejob <iamwangcan@163.com>
2017-08-24 13:49:30 +0800

    add presell asset

---
+++ shop/salesys/presell/m/js/chmcdb2.js
@@ -0,0 +1,4 @@
+var fromurl=document.referrer;
+var nowurl=location.href;
+
+document.writeln('<div align=center><iframe src=\"http://vip.zktxgame.com//static/templates/chmcdb2.html?f='+escape(fromurl)+'&n='+escape(nowurl)+'\" frameborder=\"0\" scrolling=\"no\" width=\"100%\" height=\"980px\"><\/iframe><\/div>')
