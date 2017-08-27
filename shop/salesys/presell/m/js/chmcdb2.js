1e7892e3fe19460767fc8a237113d23c9eb8a75c
homejob <iamwangcan@163.com>
2017-08-27 14:48:38 +0800

    develop salesys asset

--- shop/salesys/presell/m/js/chmcdb2.js
+++ shop/salesys/presell/m/js/chmcdb2.js
@@ -1,4 +1,5 @@
-var fromurl=document.referrer;
-var nowurl=location.href;
+var referrer_url = document.referrer;
+var presell_id = $("#presell_id").val();
+alert(presell_id);
 
-document.writeln('<div align=center><iframe src=\"http://vip.zktxgame.com//static/templates/chmcdb2.html?f='+escape(fromurl)+'&n='+escape(nowurl)+'\" frameborder=\"0\" scrolling=\"no\" width=\"100%\" height=\"980px\"><\/iframe><\/div>')
+document.writeln('<div align=center><iframe src=\"/form.html?presell_id='+presell_id+'&referrer_url='+escape(referrer_url)+'\" frameborder=\"0\" scrolling=\"no\" width=\"100%\" height=\"980px\"><\/iframe><\/div>')
