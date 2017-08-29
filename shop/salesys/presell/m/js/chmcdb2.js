1ffd9d2fbed7f01ca4ba1c32f97e10b570eecf3d
wangcanliang <iamwangcan@163.com>
2017-08-29 17:37:51 +0800

    develop salesys asset

--- shop/salesys/presell/m/js/chmcdb2.js
+++ shop/salesys/presell/m/js/chmcdb2.js
@@ -1,4 +1,4 @@
 var referrer_url = document.referrer;
 var presell_id = $("#presell_id").val();
 
-document.writeln('<div align=center><iframe src=\"/form.html?presell_id='+presell_id+'&referrer_url='+escape(referrer_url)+'\" frameborder=\"0\" scrolling=\"no\" width=\"100%\" height=\"980px\"><\/iframe><\/div>')
+document.writeln('<div align=center><iframe src=\"/form.html?presell_id='+presell_id+'&referrer_url='+escape(referrer_url)+'\" frameborder=\"0\" scrolling=\"no\" width=\"100%\" height=\"680px\"><\/iframe><\/div>')
