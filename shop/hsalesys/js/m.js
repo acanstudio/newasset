302df97a6d261c733999a2de373d93ccfa4f2996
wangcanliang <iamwangcan@163.com>
2017-12-16 08:25:33 +0800

    add hsalesys

--- shop/hsalesys/js/m.js
+++ shop/hsalesys/js/m.js
@@ -3,15 +3,16 @@
         var 
         _hideAllNonBaseMenuItem=false,
         _hideTimeline=false,
-        wx_ready=false;
-        /*wx_share={
+        _hideOptionMenu=false,
+        wx_ready=false,
+        wx_share={
             title:"FLŌWERPLUS（花+）",
             subtitle:"用鲜花点亮生活，鲜花订阅，每周一次，新客户首单送花瓶哦。",
             image:"http://static2.flowerplus.cn/Static/img/flowerpluslogo2.jpg",
             link:"http://t.flowerplus.cn",
             whenMenuShareTimeline:null,
             whenMenuShareAppMessage:null
-        };*/
+        };
 
         this.IS_WEIXIN=false;
         this.OS="PC";
@@ -23,28 +24,35 @@
           console.log('set ajaxtimeout ' + timeout);
         };
         //设置微信分享的标题
-        /*this.setWeixinShareTitle=function(v){
+        this.setWeixinShareTitle=function(v){
           $.extend(wx_share,v);
-        };*/
+        };
 
         // 隐藏微信右上角菜单
         this.hideAllNonBaseMenuItem=function(){
           _hideAllNonBaseMenuItem=true;
-          /*if(wx_ready){
+          if(wx_ready){
             wx.hideAllNonBaseMenuItem();
-          }*/
+          }
         };
 
         this.hideTimeline=function(){
           _hideTimeline=true;
-          /*if(wx_ready){
+          if(wx_ready){
             wx.hideMenuItems({
                 menuList: ["menuItem:share:timeline"] 
             });
-          }*/
+          }
         };
 
-        /*this.openWxAddress=function(onSuccess,onCancel){
+        this.hideOptionMenu =function(){
+          _hideOptionMenu=true;
+          if(wx_ready){
+            wx.hideOptionMenu();
+          }
+        };
+
+        this.openWxAddress=function(onSuccess,onCancel){
             console.log('openWxAddress');
             if(!wx_ready){
                 return;
@@ -62,30 +70,61 @@
                       }
                 }
             });
-        };*/
+        };
 
         var onWeiXinLoaded=function(v){
-          /*wx.config({
+          wx.config({
               debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
               appId: v.appId, // 必填，公众号的唯一标识
               timestamp: v.timestamp, // 必填，生成签名的时间戳
               nonceStr: v.nonceStr, // 必填，生成签名的随机串
               signature: v.signature,// 必填，签名，见附录1
               jsApiList: ['checkJsApi','onMenuShareTimeline','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','hideMenuItems','showMenuItems','hideAllNonBaseMenuItem','showAllNonBaseMenuItem','translateVoice','startRecord','stopRecord','onRecordEnd','playVoice','pauseVoice','stopVoice','uploadVoice','downloadVoice','chooseImage','previewImage','uploadImage','downloadImage','getNetworkType','openLocation','getLocation','hideOptionMenu','showOptionMenu','closeWindow','scanQRCode','chooseWXPay','openProductSpecificView','addCard','chooseCard','openCard,openAddress']
-          });*/
+          });
 
-          /*wx.ready(function(){
+          wx.ready(function(){
               wx_ready=true;
+              wx.onMenuShareTimeline({
+                  title: wx_share.title, // 分享标题
+                  link: wx_share.link, // 分享链接
+                  imgUrl: wx_share.image, // 分享图标
+                  success: function () {
+                    if(typeof wx_share.whenMenuShareTimeline == "function"){
+                        (wx_share.whenMenuShareTimeline)();
+                    }
+                  },
+                  cancel: function () { 
+                  }
+              });
+
+              wx.onMenuShareAppMessage({
+                  title: wx_share.title, // 分享标题
+                  desc: wx_share.subtitle, // 分享描述
+                  link: wx_share.link, // 分享链接
+                  imgUrl: wx_share.image, // 分享图标
+                  type: '', // 分享类型,music、video或link，不填默认为link
+                  dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                  success: function () {
+                    if(typeof wx_share.whenMenuShareAppMessage == "function"){
+                        (wx_share.whenMenuShareAppMessage)();
+                    }
+                  },
+                  cancel: function () { 
+                  }
+              });
 
               if(_hideAllNonBaseMenuItem){
                 wx.hideAllNonBaseMenuItem();
               }
               if(_hideTimeline){
-                 wx.hideMenuItems({
+                wx.hideMenuItems({
                     menuList: ["menuItem:share:timeline"] 
                 });
               }
-          });*/
+              if(_hideOptionMenu){
+                wx.hideOptionMenu();
+              }
+          });
         };
 
 
